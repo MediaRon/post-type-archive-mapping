49884341fcbfc119e2f6cef43f4a433294fe2a1b
Ronald Huereca <ronald@mediaron.com>
2026-02-19 19:24:08 -0600

    Converting main block to functional component.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7e0b01b2c45dc24edcbd');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7a8aac352f146870a269');
