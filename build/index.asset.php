233f9c14c7f0a43db3cd3cba69a0125c4ec8867a
Ronald Huereca <ronald@mediaron.com>
2026-02-19 19:34:46 -0600

    Having featured post by term into a functional component.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7a8aac352f146870a269');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '26caa07bb2adf0cc9117');
